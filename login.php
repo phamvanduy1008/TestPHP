805a03ca9300c43d2679ed1163e44e8c5a648bcb
phamvanduy1008 <phamvanduy.dev@gmail.com>
2024-12-01 11:19:30 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="../../public/style/login.css">
+</head>
+<body>
+    <div class="login-container">
+    <form method="POST" class="login-form">
+            <h2>Login</h2>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" placeholder="Enter your username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Enter your password" required>
+            </div>
+            <button type="submit" class="btn">Login</button>
+        </form>
+        <p class='text_description'>If you don't have account? <a href="./register.php">Register</a></p>
+    </div>
+</body>
+</html>
