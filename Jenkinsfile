096db8cc8931a61eda7eed7a95690a64a1085462
phamvanduy1008 <phamvanduy.dev@gmail.com>
2024-12-02 15:54:36 +0700

    update jenkins

--- Jenkinsfile
+++ Jenkinsfile
@@ -23,25 +23,6 @@ pipeline {
             }
         }
 
-        stage('Deploy MySQL to DEV') {
-            steps {
-                echo 'Deploying MySQL to DEV environment'
-                sh 'docker image pull mysql:8.0'
-                
-                sh 'docker network create dev || echo "this network exists"'
-                
-                sh 'docker container stop duyduy-mysql || echo "this container does not exist" '
-                
-                sh 'echo y | docker container prune '
-                
-                sh 'docker volume rm duyduy-mysql-data || echo "no volume"'
-
-                sh "docker run --name duyduy-mysql --rm --network dev -v duyduy-mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_LOGIN_PSW} -e MYSQL_DATABASE=db_example -d mysql:8.0"
-                sh 'sleep 20'
-
-                sh "docker exec -i duyduy-mysql mysql --user=root --password=${MYSQL_ROOT_LOGIN_PSW} < script.sql"
-            }
-        }
 
         stage('Deploy PHP App to DEV') {
             steps {
@@ -59,6 +40,26 @@ pipeline {
             }
         }
     }
+
+    //  stage('Deploy MySQL to DEV') {
+    //         steps {
+    //             echo 'Deploying MySQL to DEV environment'
+    //             sh 'docker image pull mysql:8.0'
+                
+    //             sh 'docker network create dev || echo "this network exists"'
+                
+    //             sh 'docker container stop duyduy-mysql || echo "this container does not exist" '
+                
+    //             sh 'echo y | docker container prune '
+                
+    //             sh 'docker volume rm duyduy-mysql-data || echo "no volume"'
+
+    //             sh "docker run --name duyduy-mysql --rm --network dev -v duyduy-mysql-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_LOGIN_PSW} -e MYSQL_DATABASE=db_example -d mysql:8.0"
+    //             sh 'sleep 20'
+
+    //             sh "docker exec -i duyduy-mysql mysql --user=root --password=${MYSQL_ROOT_LOGIN_PSW} < script.sql"
+    //         }
+    //     }
 
     post {
         always {
--- src/index.php
+++ src/index.php
@@ -1,49 +1,50 @@
 <?php
+echo "hello";
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 
-$host = 'localhost';
-$dbname = 'todoapp';
-$username = 'root';
-$password = '';
+// $host = 'localhost';
+// $dbname = 'todoapp';
+// $username = 'root';
+// $password = '';
 
-$conn = mysqli_connect($host, $username, $password, $dbname);
+// $conn = mysqli_connect($host, $username, $password, $dbname);
 
-if (!$conn) {
-    echo json_encode(["error" => "Connection failed: " . mysqli_connect_error()]);
-    exit;
-}
+// if (!$conn) {
+//     echo json_encode(["error" => "Connection failed: " . mysqli_connect_error()]);
+//     exit;
+// }
 
-$method = $_SERVER['REQUEST_METHOD'];
-$uri = $_SERVER['REQUEST_URI'];
+// $method = $_SERVER['REQUEST_METHOD'];
+// $uri = $_SERVER['REQUEST_URI'];
 
-$query = parse_url($uri, PHP_URL_QUERY);
-parse_str($query, $params);
+// $query = parse_url($uri, PHP_URL_QUERY);
+// parse_str($query, $params);
 
-if (isset($params['user']) && isset($params['pass']) && $method == 'GET') {
-    $user = mysqli_real_escape_string($conn, $params['user']);
-    $pass = mysqli_real_escape_string($conn, $params['pass']);
+// if (isset($params['user']) && isset($params['pass']) && $method == 'GET') {
+//     $user = mysqli_real_escape_string($conn, $params['user']);
+//     $pass = mysqli_real_escape_string($conn, $params['pass']);
 
-    $query = "INSERT INTO users (username, password, created_at, updated_at) 
-              VALUES ('$user', '$pass', NOW(), NOW())";
-    if (mysqli_query($conn, $query)) {
-        echo json_encode(["message" => "User added successfully"]);
-    } else {
-        echo json_encode(["error" => "Failed to add user: " . mysqli_error($conn)]);
-    }
-} elseif ($method == 'GET' && strpos($uri, '/all') !== false) {
-    $query = "SELECT * FROM users";
-    $result = mysqli_query($conn, $query);
+//     $query = "INSERT INTO users (username, password, created_at, updated_at) 
+//               VALUES ('$user', '$pass', NOW(), NOW())";
+//     if (mysqli_query($conn, $query)) {
+//         echo json_encode(["message" => "User added successfully"]);
+//     } else {
+//         echo json_encode(["error" => "Failed to add user: " . mysqli_error($conn)]);
+//     }
+// } elseif ($method == 'GET' && strpos($uri, '/all') !== false) {
+//     $query = "SELECT * FROM users";
+//     $result = mysqli_query($conn, $query);
 
-    if ($result) {
-        $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        echo json_encode($users);
-    } else {
-        echo json_encode(["error" => "Failed to fetch users: " . mysqli_error($conn)]);
-    }
-} else {
-    echo json_encode(["error" => "Invalid request"]);
-}
+//     if ($result) {
+//         $users = mysqli_fetch_all($result, MYSQLI_ASSOC);
+//         echo json_encode($users);
+//     } else {
+//         echo json_encode(["error" => "Failed to fetch users: " . mysqli_error($conn)]);
+//     }
+// } else {
+//     echo json_encode(["error" => "Invalid request"]);
+// }
 
-mysqli_close($conn);
+// mysqli_close($conn);
 ?>
